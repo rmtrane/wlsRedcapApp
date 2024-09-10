9a8ec0159a18d9ebecb1ea920cb2813f0922ae65
Ralph Trane <rmtrane@gmail.com>
2024-09-10 16:43:16 -0500

    Now with the ability to show all plots. Also, including shade_description for all variables available

--- R/get_description.R
+++ R/get_description.R
@@ -7,15 +7,59 @@
 #'
 #' @keywords internal
 get_descriptions <- function(raw, z_score, ss, cog_var) {
+
+  if (missingArg(raw)) {
+    raw_length <- 0
+  } else {
+    raw_length <- length(raw)
+
+    if (raw_length == 1 && is.na(raw))
+      raw_length <- 0
+  }
+
+  if (missingArg(z_score)) {
+    z_score_length <- 0
+  } else {
+    z_score_length <- length(z_score)
+
+    if (z_score_length == 1 && is.na(z_score))
+      z_score_length <- 0
+  }
+
+  if (missingArg(ss)) {
+    ss_length <- 0
+  } else {
+    ss_length <- length(ss)
+
+    if (ss_length == 1 && is.na(ss))
+      ss_length <- 0
+  }
+
+  lns <- c(raw_length, z_score_length, ss_length)
+
+  stopifnot("raw, z_score, and ss should either be NA or vectors of the same
+            length" = setdiff(lns, 0) != 1)
+
+  max_ln <- max(lns)
+
+  if (raw_length == 0)
+    raw <- rep(NA, max_ln)
+
+  if (z_score_length == 0)
+    z_score <- rep(NA, max_ln)
+
+  if (ss_length == 0)
+    ss <- rep(NA, max_ln)
+
   percentiles <- get_percentiles(z_score, ss, cog_var)
 
   dplyr::case_when(
     raw %in% c(995, 996, 997, 998, 999) ~ paste0("Unavailable (", raw, ")"),
     cog_var == "cog_cdr_global" ~ c("Normal", "Very Mild", "Mild" ,"Moderate", "Severe")[
-      findInterval(raw, c(0, 2.5, 4.5, 9.5, 16, 18.5, Inf))
+      findInterval(raw, c(2.5, 4.5, 9.5, 16, 18.5)) + 1
     ],
     cog_var == "cog_gds15" ~ c("Minimal", "Mild", "Moderate", "Severe")[
-      findInterval(raw, c(0, 5, 9, 12, 16, Inf))
+      findInterval(raw, c(5, 9, 12)) + 1
     ],
     cog_var == "cog_moca_clock" ~ rep(c("Impaired", "Normal"), c(3,1))[raw + 1],
     .default = c("Impaired",
@@ -23,8 +67,7 @@ get_descriptions <- function(raw, z_score, ss, cog_var) {
                  "Low Average",
                  "Average",
                  "High Average",
-                 "Superior")[findInterval(percentiles,
-                                          c(0, 0.03, 0.10, 0.25, 0.76, 0.92, Inf))]
+                 "Superior")[findInterval(percentiles, c(0.03, 0.10, 0.25, 0.76, 0.92)) + 1]
   )
 
 }
--- R/plotCogVarModule.R
+++ R/plotCogVarModule.R
@@ -21,19 +21,27 @@
 plotCogVarUI <- function(id) {
   shiny::tagList(
     bslib::layout_column_wrap(
-      shiny::selectizeInput(
-        shiny::NS(id, "var_to_plot"),
-        label = "Choose Variable to Plot",
-        choices = NULL
+      shiny::radioButtons(
+        inputId = shiny::NS(id, "all_or_one_plot"),
+        label = "Show all plots, or choose one?",
+        choices = c("Choose one" = "one", "Show all" = "all")
       ),
+      shiny::uiOutput(shiny::NS(id, "plot_selection")),
       shiny::selectizeInput(
         shiny::NS(id, "raw_or_standard"),
         label = "Raw, z-score or SS",
         choices = NULL
+      ),
+      #shiny::uiOutput(shiny::NS(id, "shade_descriptions"))
+      shiny::checkboxInput(
+        inputId = shiny::NS(id, "shade_descriptions"),
+        label = "Shade according to descriptions?",
+        value = FALSE
       )
     ),
-    shiny::plotOutput(shiny::NS(id, "plotCogVar"), width = "100%"),
-    shiny::uiOutput(shiny::NS(id, "shade_descriptions"))
+    shiny::uiOutput(shiny::NS(id, "plotsUI"))
+    #shiny::plotOutput(shiny::NS(id, "plotCogVar"), width = "100%")
+
   )
 }
 
@@ -84,13 +92,22 @@ plotCogVarServer <- function(id, dat, trim = Inf) {
       unique(for_var_and_scores_avail()$name)
     )
 
-    shiny::observeEvent(var_avail(), {
-      shiny::updateSelectizeInput(
-        session,
-        inputId = "var_to_plot",
-        choices = setNames(names(cog_vars_labels[var_avail()]),
-                           nm = unname(cog_vars_labels[var_avail()]))
-      )
+    shiny::observeEvent({
+      var_avail()
+      input$all_or_one_plot
+    }, {
+      if (input$all_or_one_plot == "one") {
+        output$plot_selection <- shiny::renderUI({
+          shiny::selectizeInput(
+            inputId = shiny::NS(id, "var_to_plot"),
+            label = "Choose Variable to Plot",
+            choices = setNames(names(cog_vars_labels[var_avail()]),
+                               nm = unname(cog_vars_labels[var_avail()]))
+          )
+        })
+      } else {
+        output$plot_selection <- NULL # renderUI()
+      }
     })
 
     scores_avail <- shiny::reactiveVal()
@@ -108,47 +125,99 @@ plotCogVarServer <- function(id, dat, trim = Inf) {
       )
     }, ignoreInit = T)
 
-    shiny::observeEvent(scores_avail(), {
-      shiny::updateSelectizeInput(
-        session,
-        inputId = "raw_or_standard",
-        choices = setNames(
-          stringr::str_to_lower(scores_avail()),
-          dplyr::case_match(
-            scores_avail(),
-            "raw" ~ "Raw",
-            "standardized" ~ "z-score",
-            "ss" ~ "SS",
-            .default = NA
+    shiny::observeEvent({
+      input$all_or_one_plot
+      scores_avail()
+    }, {
+
+      if (input$all_or_one_plot == "one") {
+        shiny::updateSelectizeInput(
+          session,
+          inputId = "raw_or_standard",
+          choices = setNames(
+            stringr::str_to_lower(scores_avail()),
+            dplyr::case_match(
+              scores_avail(),
+              "raw" ~ "Raw",
+              "standardized" ~ "z-score",
+              "ss" ~ "SS",
+              .default = NA
+            )
           )
         )
-      )
+      } else {
+        shiny::updateSelectizeInput(
+          session,
+          inputId = "raw_or_standard",
+          choices = c(
+            "Raw" ="raw",
+            "z-score" = "standardized",
+            "SS" = "ss"
+          )
+        )
+      }
     }, ignoreInit = T)
 
 
-    shiny::observeEvent(input$raw_or_standard, {
-      if (input$raw_or_standard == "standardized") {
-        output$shade_descriptions <- shiny::renderUI({
-          shiny::checkboxInput(
-            inputId = shiny::NS(id, "shade_descriptions"),
-            label = "Shade according to descriptions?",
-            value = FALSE
-          )
-        })
+    # shiny::observeEvent(input$raw_or_standard, {
+    #   if (input$raw_or_standard == "standardized") {
+    #     output$shade_descriptions <- shiny::renderUI({
+    #       shiny::checkboxInput(
+    #         inputId = shiny::NS(id, "shade_descriptions"),
+    #         label = "Shade according to descriptions?",
+    #         value = FALSE
+    #       )
+    #     })
+    #   } else {
+    #     output$shade_descriptions <- shiny::renderUI(shiny::markdown(""))
+    #   }
+    # })
+
+    output$plotsUI <- shiny::renderUI({
+      if (input$all_or_one_plot == "one") {
+        htmltools::tagList(
+          shiny::plotOutput(shiny::NS(id, paste(input$var_to_plot, "plot", sep = "_")),
+                            width = "100%")
+        )
       } else {
-        output$shade_descriptions <- shiny::renderUI(shiny::markdown(""))
+        if (input$raw_or_standard != "raw") {
+          ## Check if score requested exists in data, and has any non-missing values
+          tmp_dat <- dat() |>
+            dplyr::select(
+              tidyselect::any_of(paste(input$raw_or_standard, var_avail(), sep = "_"))
+            ) |>
+            dplyr::select(
+              tidyselect::where(\(x) sum(!is.na(x)) > 0)
+            )
+
+          if (ncol(tmp_dat) > 0) {
+            to_plot <- stringr::str_remove(colnames(tmp_dat), paste0(input$raw_or_standard, "_"))
+          } else {
+            to_plot <- NULL
+          }
+
+        } else {
+          to_plot <- var_avail()
+        }
+
+        if (length(to_plot) == 0) {
+          return(shiny::markdown(glue::glue("No variables with {c('standardized' = 'z-score', 'ss' = 'SS')[input$raw_or_standard]} values.")))
+        } else {
+          plot_output_list <- lapply(to_plot, function(x) {
+            shiny::plotOutput(shiny::NS(id, paste(x, "plot", sep = "_")), width = "100%")
+          })
+
+          base::do.call(htmltools::tagList, plot_output_list)
+        }
       }
     })
 
-
     shiny::observe({
+      # print(c(input$var_to_plot, paste(input$raw_or_standard, input$var_to_plot, sep = "_")))
+      # print(any(c(input$var_to_plot, paste(input$raw_or_standard, input$var_to_plot, sep = "_")) %in% colnames(dat())))
 
-      print(c(input$var_to_plot, paste(input$raw_or_standard, input$var_to_plot, sep = "_")))
-      print(any(c(input$var_to_plot, paste(input$raw_or_standard, input$var_to_plot, sep = "_")) %in% colnames(dat())))
-
-      if (any(c(input$var_to_plot, paste(input$raw_or_standard, input$var_to_plot, sep = "_")) %in% colnames(dat()))) {
-
-        output$plotCogVar <- shiny::renderPlot({
+      if (input$all_or_one_plot == "one" & any(c(input$var_to_plot, paste(input$raw_or_standard, input$var_to_plot, sep = "_")) %in% colnames(dat()))) {
+        output[[paste(input$var_to_plot, "plot", sep = "_")]] <- shiny::renderPlot({
           plot_cog_var(
             dat(),
             var_to_plot = input$var_to_plot,
@@ -160,8 +229,68 @@ plotCogVarServer <- function(id, dat, trim = Inf) {
               title = cog_vars_labels[input$var_to_plot]
             )
         })
+      } else {
+
+        print(names(output))
+
+        for (var in var_avail()) {
+          local({
+            my_var <- var
+
+            # If we're plotting not-raw scores, are there any non-missing values
+            # for this variable?
+            if (input$raw_or_standard != 'raw') {
+              tmp_dat <- dat() |>
+                dplyr::select(tidyselect::any_of(paste(input$raw_or_standard, my_var, sep = "_"))) |>
+                dplyr::select(
+                  tidyselect::where(\(x) sum(!is.na(x)) > 0)
+                )
+
+              no_obs <- ncol(tmp_dat) == 0
+            }
+
+            if (input$raw_or_standard != "raw" & no_obs) {
+
+              output[[paste(my_var, "plot", sep = "_")]] <- shiny::renderPlot({
+                ggplot2::ggplot() +
+                  ggplot2::ggtitle(cog_vars_labels[my_var]) +
+                  #marquee::geom_marquee(
+                  ggplot2::geom_text(
+                    ggplot2::aes(
+                      x = I(0.5),
+                      y = I(0.5),
+                      label = paste(c("standardized" = "z-score", "ss" = "SS")[input$raw_or_standard],
+                                    "values not available for",
+                                    cog_vars_labels[my_var])
+                    )
+                  ) +
+                  ggplot2::theme_minimal() +
+                  ggplot2::theme(axis.title = ggplot2::element_blank())
+              })
+            } else {
+              output[[paste(my_var, "plot", sep = "_")]] <- shiny::renderPlot({
+                plot_cog_var(
+                  dat(),
+                  var_to_plot = my_var,
+                  type = input$raw_or_standard,
+                  shade_descriptions = input$shade_descriptions,
+                  trim = trim
+                ) +
+                  ggplot2::labs(
+                    title = cog_vars_labels[my_var]
+                  )
+              })
+            }
+          })
+        }
       }
     })
+
+    # output$plotCogVar <- shiny::renderUI({
+    #   htmltools::tagList(
+    #     map(plots(), \(x) shiny::renderPlot(x))
+    #   )
+    # })
 
   })
 }
@@ -179,7 +308,7 @@ plotCogVarApp <- function(dat, trim = Inf) {
   )
 
   server <- function(input, output, session) {
-    plotCogVarServer("plot_cog_var", reactive(dat), trim)
+    plotCogVarServer("plot_cog_var", shiny::reactive(dat), trim)
   }
 
   shiny::shinyApp(ui, server)
